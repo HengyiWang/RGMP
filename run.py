22345d6aef51b7aee6407f4fd5e36126e5e8e729
wanghy <wanghengyi@gmail.com>
2020-10-03 14:42:58 +0800

    Update run.py

--- run.py
+++ run.py
@@ -9,7 +9,6 @@ import torch.nn.init as init
 import torch.utils.model_zoo as model_zoo
 from torchvision import models
 
-
 # general libs
 import cv2
 from PIL import Image
@@ -21,24 +20,24 @@ import time
 from model import RGMP
 from utils import ToCudaVariable, ToLabel, DAVIS, upsample, downsample
 
-
-
 ### set pathes
-DAVIS_ROOT = '../data/davis-2017/data/DAVIS/'
+DAVIS_ROOT = '/content/DAVIS/'
 palette = Image.open(DAVIS_ROOT + 'Annotations/480p/bear/00000.png').getpalette()
+
 
 def get_arguments():
     parser = argparse.ArgumentParser(description="RGMP")
     parser.add_argument("-MO", action="store_true", help="Multi-object")
     return parser.parse_args()
+
+
 args = get_arguments()
-MO = args.MO 
+MO = args.MO
 
 if MO:
     print('Multi-object VOS on DAVIS-2017 valildation')
 else:
     print('Single-object VOS on DAVIS-2016 valildation')
-
 
 
 def Encode_MS(val_F1, val_P1, scales):
@@ -54,6 +53,7 @@ def Encode_MS(val_F1, val_P1, scales):
 
     return ref
 
+
 def Propagate_MS(ref, val_F2, val_P2, scales):
     h, w = val_F2.size()[2], val_F2.size()[3]
     msv_E2 = {}
@@ -61,14 +61,14 @@ def Propagate_MS(ref, val_F2, val_P2, scales):
         if sc != 1.0:
             msv_F2, msv_P2 = downsample([val_F2, val_P2], sc)
             msv_F2, msv_P2 = ToCudaVariable([msv_F2, msv_P2], volatile=True)
-            r5, r4, r3, r2  = model.module.Encoder(msv_F2, msv_P2)
+            r5, r4, r3, r2 = model.module.Encoder(msv_F2, msv_P2)
             e2 = model.module.Decoder(r5, ref[sc], r4, r3, r2)
-            msv_E2[sc] = upsample(F.softmax(e2[0], dim=1)[:,1].data.cpu(), (h,w))
+            msv_E2[sc] = upsample(F.softmax(e2[0], dim=1)[:, 1].data.cpu(), (h, w))
         else:
             msv_F2, msv_P2 = ToCudaVariable([val_F2, val_P2], volatile=True)
-            r5, r4, r3, r2  = model.module.Encoder(msv_F2, msv_P2)
+            r5, r4, r3, r2 = model.module.Encoder(msv_F2, msv_P2)
             e2 = model.module.Decoder(r5, ref[sc], r4, r3, r2)
-            msv_E2[sc] = F.softmax(e2[0], dim=1)[:,1].data.cpu()
+            msv_E2[sc] = F.softmax(e2[0], dim=1)[:, 1].data.cpu()
 
     val_E2 = torch.zeros(val_P2.size())
     for sc in scales:
@@ -79,61 +79,58 @@ def Propagate_MS(ref, val_F2, val_P2, scales):
 
 def Infer_SO(all_F, all_M, num_frames, scales=[0.5, 0.75, 1.0]):
     all_E = torch.zeros(all_M.size())
-    all_E[:,:,0] = all_M[:,:,0]
+    all_E[:, :, 0] = all_M[:, :, 0]
 
-    ref = Encode_MS(all_F[:,:,0], all_E[:,0,0], scales)
-    for f in range(0, num_frames-1):
-        all_E[:,0,f+1] = Propagate_MS(ref, all_F[:,:,f+1], all_E[:,0,f], scales)
+    ref = Encode_MS(all_F[:, :, 0], all_E[:, 0, 0], scales)
+    for f in range(0, num_frames - 1):
+        all_E[:, 0, f + 1] = Propagate_MS(ref, all_F[:, :, f + 1], all_E[:, 0, f], scales)
 
     return all_E
-
 
 
 def Infer_MO(all_F, all_M, num_frames, num_objects, scales=[0.5, 0.75, 1.0]):
     if num_objects == 1:
-        obj_E =  Infer_SO(all_F, all_M, num_frames, scales=scales) #1,1,t,h,w
-        return torch.cat([1-obj_E, obj_E], dim=1)
+        obj_E = Infer_SO(all_F, all_M, num_frames, scales=scales)  # 1,1,t,h,w
+        return torch.cat([1 - obj_E, obj_E], dim=1)
 
     _, n, t, h, w = all_M.size()
-    all_E = torch.zeros((1,n+1,t,h,w))
-    all_E[:,1:,0] = all_M[:,:,0]
-    all_E[:,0,0] = 1-torch.sum(all_M[:,:,0], dim=1)
+    all_E = torch.zeros((1, n + 1, t, h, w))
+    all_E[:, 1:, 0] = all_M[:, :, 0]
+    all_E[:, 0, 0] = 1 - torch.sum(all_M[:, :, 0], dim=1)
 
-    ref_bg = Encode_MS(all_F[:,:,0], torch.sum(all_E[:,1:,0], dim=1), scales)
+    ref_bg = Encode_MS(all_F[:, :, 0], torch.sum(all_E[:, 1:, 0], dim=1), scales)
     refs = []
     for o in range(num_objects):
-        refs.append(Encode_MS(all_F[:,:,0], all_E[:,o+1,0], scales))
+        refs.append(Encode_MS(all_F[:, :, 0], all_E[:, o + 1, 0], scales))
 
-    for f in range(0, num_frames-1):
+    for f in range(0, num_frames - 1):
         ### 1 - all 
-        all_E[:,0,f+1] = 1-Propagate_MS(ref_bg, all_F[:,:,f+1], torch.sum(all_E[:,1:,f], dim=1), scales)
+        all_E[:, 0, f + 1] = 1 - Propagate_MS(ref_bg, all_F[:, :, f + 1], torch.sum(all_E[:, 1:, f], dim=1), scales)
         for o in range(num_objects):
-            all_E[:,o+1,f+1] = Propagate_MS(refs[o], all_F[:,:,f+1], all_E[:,o+1,f], scales)
+            all_E[:, o + 1, f + 1] = Propagate_MS(refs[o], all_F[:, :, f + 1], all_E[:, o + 1, f], scales)
 
         # Normalize by softmax
-        all_E[:,:,f+1] = torch.clamp(all_E[:,:,f+1], 1e-7, 1-1e-7)
-        all_E[:,:,f+1] = torch.log((all_E[:,:,f+1] /(1-all_E[:,:,f+1])))
-        all_E[:,:,f+1] = F.softmax(Variable(all_E[:,:,f+1]), dim=1).data
+        all_E[:, :, f + 1] = torch.clamp(all_E[:, :, f + 1], 1e-7, 1 - 1e-7)
+        all_E[:, :, f + 1] = torch.log((all_E[:, :, f + 1] / (1 - all_E[:, :, f + 1])))
+        all_E[:, :, f + 1] = F.softmax(Variable(all_E[:, :, f + 1]), dim=1).data
 
     return all_E
 
 
-
 if MO:
-    Testset = DAVIS(DAVIS_ROOT, imset='2017/val.txt', multi_object=True)
+    Testset = DAVIS(DAVIS_ROOT, imset='1080p/val.txt', multi_object=True)
     Testloader = data.DataLoader(Testset, batch_size=1, shuffle=False, num_workers=2, pin_memory=True)
 else:
-    Testset = DAVIS(DAVIS_ROOT, imset='2016/val.txt')
+    Testset = DAVIS(DAVIS_ROOT, imset='480p/val.txt')
     Testloader = data.DataLoader(Testset, batch_size=1, shuffle=False, num_workers=2, pin_memory=True)
 
 model = nn.DataParallel(RGMP())
 if torch.cuda.is_available():
     model.cuda()
 
-
 model.load_state_dict(torch.load('weights.pth'))
 
-model.eval() # turn-off BN
+model.eval()  # turn-off BN
 for seq, (all_F, all_M, info) in enumerate(Testloader):
     all_F, all_M = all_F[0], all_M[0]
     seq_name = info['name'][0]
@@ -142,26 +139,41 @@ for seq, (all_F, all_M, info) in enumerate(Testloader):
 
     tt = time.time()
     all_E = Infer_MO(all_F, all_M, num_frames, num_objects, scales=[0.5, 0.75, 1.0])
-    print('{} | num_objects: {}, FPS: {}'.format(seq_name, num_objects, (time.time()-tt)/num_frames))
+    print('{} | num_objects: {}, FPS: {}'.format(seq_name, num_objects, (time.time() - tt) / num_frames))
 
     # Save results for quantitative eval ######################
     if MO:
         folder = 'results/MO'
     else:
-        folder = 'results/SO'    
+        folder = 'results/SO'
     test_path = os.path.join(folder, seq_name)
     if not os.path.exists(test_path):
         os.makedirs(test_path)
 
     for f in range(num_frames):
-        E = all_E[0,:,f].numpy()
+        E = all_E[0, :, f].numpy()
+        E = ToLabel(E)
+        # print(E.shape)
+        E = E * 255
+
+        cv2.imwrite(os.path.join(test_path, '{:05d}.png'.format(f)), E)
+        """h, w, c = E.shape
+        for h0 in range(0,h):
+          for w0 in range(0,w):
+            for c0 in range(0,c):
+              if(E[h0,w0,c0]>0):
+                print(E[h0,w0,c0])
+              else:
+                print("no")
+
         # make hard label
         E = ToLabel(E)
-    
+
         (lh,uh), (lw,uw) = info['pad'] 
         E = E[lh[0]:-uh[0], lw[0]:-uw[0]]
-
-        img_E = Image.fromarray(E)
+        #cv2.imwrite(os.path.join(test_path, '{:05d}.png'.format(f)), E)
+        img_E = Image.fromarray(E).convert('P')
+        img_E.load()
         img_E.putpalette(palette)
-        img_E.save(os.path.join(test_path, '{:05d}.png'.format(f)))
+        img_E.save(os.path.join(test_path, '{:05d}.png'.format(f)))"""
 
